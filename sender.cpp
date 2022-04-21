d63a92f35e26d64557301b824148116f78a1315d
zyan <zbaptis1@jh.edu>
2022-04-20 23:42:17 -0400

    made progress with sender.cpp

--- sender.cpp
+++ sender.cpp
@@ -22,12 +22,25 @@ int main(int argc, char **argv) {
   // TODO: connect to server
   Connection conn;
   conn.connect(server_hostname, server_port); // use sever socket to connect to server
-
+  if (!conn.is_open()) {
+    std::cerr << "Couldn't connect to server\n";
+    return 1;
+  }
+  
   // TODO: send slogin message
-  Message slogin(TAG_SLOGIN, "/slogin " + username); 
+  Message slogin(TAG_SLOGIN, username); 
   if (!conn.send(slogin)) { //Error sending login
-    /** TODO: figure out how to handle send error AND if needed 
-              Maybe it's printing to std::cerr??? */
+  /** TODO: is this correct */
+      std::cerr << conn.m_last_result() << endl;
+      conn.close();
+      return 1;
+  }
+
+  if (!conn.recieve(slogin)) { // Error recieving login
+  /** TODO: is this correct */
+      std::cerr << conn.m_last_result() << endl;
+      conn.close();
+      return 1;
   }
 
   // TODO: loop reading commands from user, sending messages to
@@ -41,7 +54,34 @@ int main(int argc, char **argv) {
         - end command to end while loop
     */
 
+    // Format:> [insert user input] \n
+    std::cout << "> ";
+    std::string line; // user input
 
+    std::getLine(std::cin, line);
+
+    // why trim??
+    
+    if (line[0] == '/') {
+      std::string command = line.substr(1, line.length());
+      // join
+      if (line.substr(1, 5) == "join") {
+        std::string roomName;
+        if (line.substr(5, line.length()) == "") {
+          std::cerr << "Needs a room name: ./join [room name]\n";
+        }
+      } else if (line.substr(1, 6) == "leave") { // leave
+        // send message to signify leave
+      } else if (line.substr(1, 5) == "quit") { // quit
+        // send message to signify quit
+        break;
+      } else {
+        std::cerr << "-Invalid command-\n" << "Valid Commands: /join /leave /quit\n"; 
+      }
+    }
+
+    // what is send_request?
   }
+  conn.close();
   return 0;
 }
