b4ca204fce68729aba0680f9cb1f1eb249159669
Zyan Baptiste <zyanb@zyans-mbp.acasmart.jh.edu>
2022-05-02 13:19:05 -0400

    added chat with server, working on error checking throughout file

--- server.cpp
+++ server.cpp
@@ -74,6 +74,7 @@ namespace {
     //       separate helper functions for each of these possibilities
     //       is a good idea)
 
+    delete thisUser;
     return nullptr;
   }
 }
@@ -104,7 +105,7 @@ bool Server::listen() {
   if (m_ssock < 0) { 
     std::cerr << "ERROR Couldn't listen to server" << std::endl;
     return false;
-    } 
+  } 
   
   return true;
 }
@@ -117,8 +118,7 @@ void Server::handle_client_requests() {
 
   Connection conn;
 
-  int keep_going = 1;
-  while (keep_going) {
+  while (1) {
 
     // MAYBE LAST 2 ARGS AREN'T NECESSARY: Accept(int s, struct sockaddr *addr, socklen_t *addrlen) 
     int client_fd = Accept(m_ssock, NULL, NULL);
@@ -128,9 +128,9 @@ void Server::handle_client_requests() {
       ConnInfo *info = new ConnInfo(new Connection(client_fd), this);
 
       pthread_t thr_id;
-      if (pthread_create(&thr_id, nullptr, worker, static_cast<void *>(info)) != 0) {
+      if (pthread_create(&thr_id, nullptr, worker, static_cast<void *>(info)) < 0) { // static cast? why not just info
         std::cerr << "Could not create thread\n";
-        return;
+        delete info;
       }
 
       // close(client_fd); /** TODO: see if necessary b/c worker might already handle connection closure w/ deconstructor
@@ -144,7 +144,11 @@ void Server::handle_client_requests() {
 Room *Server::find_or_create_room(const std::string &room_name) {
   // TODO: return a pointer to the unique Room object representing
   //       the named chat room, creating a new one if necessary
+  // guard g(m_lock) --> idky david has this
 
+
+  // RoomMap::iterator works, could use that be would that be sus?
+  
   if (m_rooms.find(room_name) != m_rooms.end()) return m_rooms[room_name]; //Use arrow-> reference to get value from iterator pointing to pair
   
   Room* newRoom = new Room(room_name); //Create new room if not found in map
@@ -153,49 +157,61 @@ Room *Server::find_or_create_room(const std::string &room_name) {
   return newRoom;
 }
 
-// int Server::chat_with_client(int client_fd) {
-
-//   Connection conn; /** TODO: should we pass conn. obj from server as param or create new one here */
-//   Message msg;
-
-//   // Read line from client
-//   conn.receive(msg); 
-
-//   if (rc < 0) return 1; // error reading data from client
-
-//   if (msg.tag == TAG_ERR) return 0;
-//   else {
-//     FILE *in = fmemopen(buf, (size_t) rc, "r");
-//     while (fscanf(in, "%d", &val) == 1) { sum += val; }
-//     fclose(in);
-//     snprintf(buf, sizeof(buf), "Sum is %d\n", sum);
-//     rio_writen(client_fd, buf, strlen(buf));
-//     return 1;
-//   }
-  
-// }
-
-
 void Server::chat_with_sender(User* user, Connection *conn, Server *server) {
   /** TODO: figure out implementation */
 
+  Message msg;
+  Room * room = nullptr;
 
+  while (1) {
+    if (!conn->receive(msg)) {
+      Connection::Result last_result = conn->get_last_result();
+      if (last_result == Connection::INVALID_MSG) {
+        conn->send(Message(TAG_ERR, "invalid message"));
+        // can try again
+      } else if (last_result == Connection::EOF_OR_ERROR) {
+        conn->send(Message(TAG_ERR, "EOF or Error"));
+        break;
+      }
+    }
 
-  // Just loop reading messages and sending an ok response for each one
-  // while (true) {
-  //   if (!info->conn->receive(msg)) {
-  //     if (info->conn->get_last_result() == Connection::INVALID_MSG) {
-  //       info->conn->send(Message(TAG_ERR, "invalid message"));
-  //     }
-  //     break;
-  //   }
+    //  join
+    if (msg.tag == TAG_JOIN) {
+      std::string roomName = msg.data;
+      // check for valid room name
+      if (room != nullptr) { room->remove_member(user); } // sender is in a room already
+      room = server->find_or_create_room(roomName);
+      room->add_member(user);
+      conn->send(Message(TAG_OK, "succesfully joined " + roomName + "!"));
+    }
+    // leave
+    else if (msg.tag == TAG_LEAVE) {
+      if (room == nullptr) { conn->send(Message(TAG_ERR, "not in any room")); }
+      else {
+        room->remove_member(user);
+        conn->send(Message(TAG_OK, user->username + " has left the room " + room->get_room_name()));
+        room = nullptr;
+      }
+    }
+    // quit
+    else if (msg.tag == TAG_QUIT) {
+      conn->send(Message(TAG_OK, "quit"));
+      break;
+    }
+    // sendall
+    else if (msg.tag == TAG_SENDALL) {
+      if (room == nullptr) { conn->send(Message(TAG_ERR, "not in any room")); }
+      else {
+        room->broadcast_message(user->username, msg.data);
+        conn->send(Message(TAG_OK, "message was sent to all users"));
+      }
+    }
+    // invalid message tag
+    else { conn->send(Message(TAG_ERR, "invalid message tag")); }
 
-  //   if (!info->conn->send(Message(TAG_OK, "this is just a dummy response"))) {
-  //     break;
-  //   }
-  // }
-
-
+  }
+    // close the room as the user is done using it
+    if (room) { room->remove_member(user); }
 }
 
 
@@ -211,17 +227,32 @@ void Server::chat_with_receiver(User *user, Connection *conn, Server *server) {
     Connection::Result last_result = conn->get_last_result();
     if (last_result == Connection::INVALID_MSG) {
       conn->send(Message(TAG_ERR, "invalid message"));
+      return;
     } else if (last_result == Connection::EOF_OR_ERROR) {
     /** TODO: send error */
+      conn->send(Message(TAG_ERR, "EOF or Error"));
+      return;
     }
   }
 
+  if (msg.tag != TAG_JOIN) {
+    conn->send(Message(TAG_ERR, "need to send a join message first"));
+    return;
+  }
+
+
+  std::string room_name = msg.data;
+  if (room_name) {  // add room error checking 
+    conn->send(Message(TAG_ERR, "invalid room name"));
+    return;
+  }
 
   /** TODO: ADD OTHER ERROR CHECKING */
 
-  std::string room_name = msg.data;
   Room *room = server->find_or_create_room(room_name);
   room->add_member(user);
+
+  conn->send(Message(TAG_OK, "succesfully joined room"));
 
 
   Message *newMsg;
